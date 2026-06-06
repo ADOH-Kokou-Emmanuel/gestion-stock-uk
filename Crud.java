5c235e9c7b5673e45c73e4d74b68ea0b07e1a0cc
Jean Urbain Koku Dziedzom KUMAKO <jeanur191207@gmail.com>
2026-06-06 11:21:46 +0000

    Modifier gestion de stock uk0

--- Crud.java
+++ Crud.java
@@ -15,6 +15,41 @@ public class Crud {
     // 2- Modifier : une méthode ModifierProduit(String pNomProduit,int
     // pQuantite,double pPrix) qui ne retourne rien (void)
 
+// Getters
+public String getNomProduit() {
+    return nomProduit;
+}
+
+public int getQuantite() {
+    return quantite;
+}
+
+public double getPrix() {
+    return prix;
+}
+
+// Setters
+public void setNomProduit(String nomProduit) {
+    this.nomProduit = nomProduit;
+}
+
+public void setQuantite(int quantite) {
+    this.quantite = quantite;
+}
+
+public void setPrix(double prix) {
+    this.prix = prix;
+}
+
+// Modifier
+public void modifierProduit(String pNomProduit, int pQuantite, double pPrix) {
+    setNomProduit(pNomProduit);
+    setQuantite(pQuantite);
+    setPrix(pPrix);
+}
+
+    
+
     // 3- Supprimer: une méthode supprimerProduit() qui ne retourne rien(void)
 
     // 4- Afficher: une méthode afficherProduit() qui ne retourne rien(void)
